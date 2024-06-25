e325bbe6049483d68c461dc1dba29119c215fd6a
Soltész Márk <mark.soltesz@shoprenter.hu>
2024-06-25 20:00:29 +0200

    luasnip keybinds

--- .config/nvim/lua/plugins/completions.lua
+++ .config/nvim/lua/plugins/completions.lua
@@ -18,6 +18,13 @@ return {
 			local cmp = require("cmp")
 			local luasnip = require("luasnip")
 
+			vim.keymap.set({ "i", "s" }, "<Tab>", function()
+				luasnip.jump(1)
+			end, { silent = true })
+			vim.keymap.set({ "i", "s" }, "<S-Tab>", function()
+				luasnip.jump(-1)
+			end, { silent = true })
+
 			require("luasnip.loaders.from_vscode").lazy_load()
 
 			luasnip.filetype_extend("templ", { "html" })
