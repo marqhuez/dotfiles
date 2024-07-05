8093fe60dcf00a0fe5db5f92d3fba84f6c6e0180
Soltész Márk <mark.soltesz@shoprenter.hu>
2024-07-05 13:33:26 +0200

    dap wip

---
+++ .config/nvim/lua/plugins/dap.lua
@@ -0,0 +1,41 @@
+return {
+	"mfussenegger/nvim-dap",
+	dependencies = {
+		"nvim-neotest/nvim-nio",
+		"rcarriga/nvim-dap-ui",
+	},
+	config = function()
+		local dap, dapui = require("dap"), require("dapui")
+
+		dap.listeners.before.attach.dapui_config = function()
+			dapui.open()
+		end
+		dap.listeners.before.launch.dapui_config = function()
+			dapui.open()
+		end
+		dap.listeners.before.event_terminated.dapui_config = function()
+			dapui.close()
+		end
+		dap.listeners.before.event_exited.dapui_config = function()
+			dapui.close()
+		end
+
+		vim.keymap.set("n", "<leader>dt", dap.toggle_breakpoint, { desc = "Toggle breakpoint" })
+		vim.keymap.set("n", "<leader>dc", dap.continue, { desc = "Continue debug" })
+
+		dap.adapters.php = {
+			type = "executable",
+			command = "node",
+			args = { "~/xdebug/out/phpDebug.js" },
+		}
+
+		dap.configurations.php = {
+			{
+				type = "php",
+				request = "launch",
+				name = "Listen for Xdebug",
+				port = 9000,
+			},
+		}
+	end,
+}
