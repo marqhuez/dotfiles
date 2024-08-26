9ac624ba1785b1f7fcc7f75b54961f9e6b381003
Soltész Márk <mark.soltesz@shoprenter.hu>
2024-08-26 14:22:28 +0200

    other cutlass

--- .config/nvim/lua/plugins/cutlass.lua
+++ .config/nvim/lua/plugins/cutlass.lua
@@ -1,9 +1,14 @@
 return {
-	"svermeulen/vim-cutlass",
+	"gbprod/cutlass.nvim",
 	config = function()
 		vim.keymap.set("n", "x", "d", { desc = "Cut" })
-		vim.keymap.set("n", "xx", "dd", { desc = "Cut" })
-		vim.keymap.set("v", "x", "d", { desc = "Cut" })
 		vim.keymap.set("x", "x", "d", { desc = "Cut" })
+		vim.keymap.set("n", "xx", "dd", { desc = "Cut" })
+		vim.keymap.set("n", "X", "D", { desc = "Cut" })
+		vim.keymap.set("v", "x", "d", { desc = "Cut" })
+
+		require("cutlass").setup({
+			exclude = { "ns", "nS", "vs", "vS" },
+		})
 	end,
 }
