4ae933e37727f43f876aa48fbc0f7c7c195d481c
Márk Soltész <soltesz.mark9876@gmail.com>
2026-01-26 12:34:25 +0100

    colors

--- .config/nvim/lua/plugins/onedark.lua
+++ .config/nvim/lua/plugins/onedark.lua
@@ -10,5 +10,9 @@ return {
 
 		vim.api.nvim_set_hl(0, "NvimTreeRootFolder", { fg = "#A9B0B8" })
 		vim.api.nvim_set_hl(0, "NvimTreeFolderIcon", { fg = "#4FA6ED" })
+
+    -- vim.api.nvim_set_hl(0, "@lsp.type.property", { fg = "#cdd6f4" })
+    -- vim.api.nvim_set_hl(0, "@lsp.type.property.dart", { link = "@lsp.type.property" })
+    vim.api.nvim_set_hl(0, "@lsp.type.property.dart", { fg = "#E55561" })
 	end,
 }
